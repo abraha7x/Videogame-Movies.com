865c798cc6409947ad02ccf79326bdb4082cf140
abraha7x <miten.mitsurugi@gmail.com>
2018-11-07 18:27:43 -0600

    new pages

--- script_menu.js
+++ script_menu.js
@@ -1,23 +1,13 @@
 $(document).ready(function() {
-
     $("#open_menu").click(function() {
-
         $("#menu").animate({
-
             "right": "0"
-
         });
-
     });
 
     $("#close_menu").click(function() {
-
         $("#menu").animate({
-
             "right": "-100%"
-
         });
-
     });
-
 });
